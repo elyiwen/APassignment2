f8eaf01573289855fff1a90e5ea2958085be55d5
Tayyun <133857703+Tayyun@users.noreply.github.com>
2023-06-12 20:29:10 +0800

    create LoginScene

---
+++ EMR/src/LoginSceneController.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginSceneController {
+    
+    @FXML
+    private Button btnLogIn;
+
+    @FXML
+    private Button btnAdmin;
+
+    @FXML
+    private TextField tfUsername;
+
+    @FXML
+    private TextField tfPassword;
+
+    @FXML
+    public void btnLogInClicked(ActionEvent e) throws IOException,ClassNotFoundException{
+        
+    }
+
+    @FXML
+    public void btnAdminClicked(ActionEvent e) throws IOException, ClassNotFoundException{
+        
+    }
+}
