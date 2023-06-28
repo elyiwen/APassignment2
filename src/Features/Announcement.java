4c13fe0f01883758918f7022554c49110ea8e396
Loh Yi Wen <yloh0008@student.monash.edu>
2023-06-28 17:23:45 +0800

    read file fix

--- src/Features/Announcement.java
+++ src/Features/Announcement.java
@@ -7,9 +7,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Announcement {
+public class Announcement implements Serializable{
 
     private String date;
     private String announcementMessage;
